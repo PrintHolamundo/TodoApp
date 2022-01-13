accf95f4505673514f4700569951633c1e5a7212
PrintHolamundo <jdohca@gmail.com>
2022-01-13 13:15:53 -0600

    Construir las tareas en el HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './styles.css';
 import {Todo,TodoList} from './classes';
+import {crearTodoHtml} from './js/componentes';
 
 const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('Comprar un unicornio');
+const tarea = new Todo('Aprender JavaScript!!');
+
 
 todoList.nuevaTodo(tarea);
-todoList.nuevaTodo(tarea2);
+
 
 console.log(todoList);
+
+crearTodoHtml(tarea);
