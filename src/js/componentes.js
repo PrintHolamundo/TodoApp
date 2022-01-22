f1f657283d7a7bc08727d73e6c0929f38c25766a
PrintHolamundo <jdohca@gmail.com>
2022-01-22 11:04:34 -0600

    Marcar como completado un Todo

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -7,27 +7,27 @@ const txtInput = document.querySelector('.new-todo');
 
 
 export const crearTodoHtml = (todo) => {
-  const htmlTodo = `
-    <li class="${(todo.completado)? 'completed' : ''}" data-id="${todo.id}">
+	const htmlTodo = `
+    <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
 						<div class="view">
-							<input class="toggle" type="checkbox" ${(todo.completado)? 'checked' : ''}>
-							<label>${ todo.tarea }</label>
+							<input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
+							<label>${todo.tarea}</label>
 							<button class="destroy"></button>
 						</div>
 						<input class="edit" value="Create a TodoMVC template">
 	</li>`;
-    const div = document.createElement('div');
-    div.innerHTML = htmlTodo;
+	const div = document.createElement('div');
+	div.innerHTML = htmlTodo;
 
-    divTodoList.append(div.firstElementChild);
-    
-    return div;
+	divTodoList.append(div.firstElementChild);
+
+	return div;
 };
 
 
 //Eventos 
 txtInput.addEventListener('keyup', (event) => {
-	  if(event.keyCode === 13 && txtInput.value.length > 0){
+	if (event.keyCode === 13 && txtInput.value.length > 0) {
 
 		const nuevoTodo = new Todo(txtInput.value);
 		todoList.nuevoTodo(nuevoTodo);
@@ -35,5 +35,16 @@ txtInput.addEventListener('keyup', (event) => {
 
 		crearTodoHtml(nuevoTodo);
 		txtInput.value = '';
-  }
+	}
 });
+
+divTodoList.addEventListener('click', (event) => {
+	const nombreElemento = event.target.localName;
+	const todoElemento = event.target.parentElement.parentElement;
+	const todoId = todoElemento.getAttribute('data-id');
+
+	if (nombreElemento.includes('input')) {
+		todoList.marcarCompletado(todoId);
+		todoElemento.classList.toggle('completed');
+	}
+})
