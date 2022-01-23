7c75ed07aed0745ba5102ef26560a6be13908966
PrintHolamundo <jdohca@gmail.com>
2022-01-23 13:52:42 -0600

    Eliminar Todos completados

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,6 +4,8 @@ import { todoList } from '../index';
 //Referencias en el html element
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+
 
 
 export const crearTodoHtml = (todo) => {
@@ -46,8 +48,25 @@ divTodoList.addEventListener('click', (event) => {
 	if (nombreElemento.includes('input')) {
 		todoList.marcarCompletado(todoId);
 		todoElemento.classList.toggle('completed');
-	} else if(nombreElemento.includes('button')){
+	} else if (nombreElemento.includes('button')) {
 		todoList.eliminarTodo(todoId);
 		divTodoList.removeChild(todoElemento);
 	}
+});
+
+btnBorrar.addEventListener('click', () => {
+
+		todoList.eliminarCompletados();
+
+		for (let i = divTodoList.children.length-1; i >= 0; i--)
+		{
+			const elemento = divTodoList.children[i];
+			
+			if(elemento.classList.contains('completed'))
+			{
+				divTodoList.removeChild(elemento);
+			}
+		}
+
+
 })
